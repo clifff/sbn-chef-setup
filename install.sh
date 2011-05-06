f1e41c9a46318e46f1d76e9ef574143d3151d77d
Skip Baney <twelvelabs@gmail.com>
2011-05-06 17:41:43 -0500

    trying to fix the install script

--- install.sh
+++ install.sh
@@ -8,15 +8,24 @@ COOKBOOKS_DIR="$REPO_DIR/cookbooks-vendor"
 COOKBOOKS_URL="http://github.com/twelvelabs/osx-cookbooks/tarball/master"
 
 
-# unpack the repo into $REPO_DIR...
+echo ""
+echo "unpacking <http://github.com/twelvelabs/chef-repo> into '$REPO_DIR'..."
 rm -Rf $REPO_DIR
 mkdir -p $REPO_DIR
 curl -sL $REPO_URL | tar -xz -C $REPO_DIR -m --strip 1
 
-# unpack the third party cookbooks
+
+echo "unpacking <http://github.com/twelvelabs/osx-cookbooks> into '$COOKBOOKS_DIR'..."
 rm -Rf $COOKBOOKS_DIR
 mkdir -p $COOKBOOKS_DIR
 curl -sL $COOKBOOKS_URL | tar -xz -C $COOKBOOKS_DIR -m --strip 1
 
-# get to cookin'...
-cd $REPO_DIR && rake chef
+echo "running chef..."
+echo ""
+
+cd $REPO_DIR
+rake chef
+
+echo ""
+echo "fini!"
+echo ""
