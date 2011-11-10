1005ee92198c360ff786aa81583af957093360b6
Clif Reeder <clif@sbnation.com>
2011-11-10 18:23:47 -0500

    added sbn stuff to install.sh

--- install.sh
+++ install.sh
@@ -27,5 +27,29 @@ cd $REPO_DIR
 rake chef
 
 echo ""
+echo "installing sbn RVM gemset..."
+rvm install ree-1.8.7-2010.02
+rvm use ree-1.8.7-2010.02@sbn --create
+rvm use ree-1.8.7-2010.02@sbn --default
+gem update --system 1.3.7
+gem install bundler -v 1.0.15
+
+# TODO mysql my.cnf stuff here
+# TODO git name/email
+
+echo ""
+echo "installing various things via homebrew..."
+brew install aspell --lang=en
+brew install memcached
+brew install redis
+brew install ImageMagick
+brew install libxml2 --with-xml2-config
+brew link libxml2
+end
+
+echo "installing mysql gem"
+env ARCHFLAGS="-arch x86_64" gem install mysql -- --with-mysql-config=/usr/local/mysql/bin/mysql_config
+
+echo ""
 echo "fini!"
 echo ""
