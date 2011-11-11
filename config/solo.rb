52c0fc5ecceed49bbbaae0bc866938ef1fd4850c
Clif Reeder <clifreeder@gmail.com>
2011-11-11 10:42:23 -0500

    Update config/solo.rb

--- config/solo.rb
+++ config/solo.rb
@@ -1,4 +1,4 @@
 REPO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 file_cache_path REPO_ROOT
-cookbook_path [File.join(REPO_ROOT, "cookbooks-vendor"), File.join(REPO_ROOT, "cookbooks")]
+cookbook_path [File.join(REPO_ROOT, "cookbooks")]
