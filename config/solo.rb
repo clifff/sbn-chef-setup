1c87e90fce39fd3483ac5a87a1e22f5e5943edcd
Clif Reeder <clifreeder@gmail.com>
2011-11-11 13:56:27 -0500

    Move chef cache to $HOME too

--- config/solo.rb
+++ config/solo.rb
@@ -4,3 +4,8 @@ REPO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 # in order to make homebrew/rvm/etc happy
 file_cache_path File.join(ENV['HOME'], 'chef-solo')
 cookbook_path [File.join(REPO_ROOT, "cookbooks")]
+
+# and since we aren't root, specify where cache goes
+local_cache = File.join(ENV['HOME'], '.cache', 'chef-solo')
+Chef::Config[:cache_options][:path] = File.join(local_cache, 'cache')
+Chef::Config[:file_backup_path] = File.join(local_cache, 'backup')
