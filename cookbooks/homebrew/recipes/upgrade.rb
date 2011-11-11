495142b414ccefcec38d3515b888ed92aeac85e9
Clif Reeder <clif@sbnation.com>
2011-11-11 10:20:48 -0500

    Trying to cleanup homebrew cookbook

--- cookbooks/homebrew/recipes/upgrade.rb
+++
@@ -1,8 +0,0 @@
-include_recipe "homebrew"
-
-`#{node[:homebrew][:prefix]}/bin/brew outdated`.lines.each do |line|
-  package line.chomp do
-    action :upgrade
-    notifies :run, resources(:execute => "brew cleanup"), :delayed
-  end
-end
