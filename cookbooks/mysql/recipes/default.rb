fc3d5c7173d4c455a32d5d9c9ed9f6c0abe0ba31
Clif Reeder <clifreeder@gmail.com>
2011-11-11 15:02:38 -0500

    Tweaking mysql homebrew install process

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -3,6 +3,7 @@ include_recipe "homebrew"
 package "mysql"
 
 execute "mysql_install_db" do
+  command "mysql_install_db  --user=mysql --tmpdir=/tmp && mysql.server start && mysql_secure_installation"
   user node[:homebrew][:user]
   creates "#{node[:homebrew][:prefix]}/var/mysql"
 end
