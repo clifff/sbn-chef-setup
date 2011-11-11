1984fc3f0acee6f9a88cfff4365f216ffe6f57a0
Clif Reeder <clifreeder@gmail.com>
2011-11-11 13:39:14 -0500

    Move file_cache from tmp (still goes to /private) to HOME

--- config/solo.rb
+++ config/solo.rb
@@ -2,5 +2,5 @@ REPO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 # Using /tmp instead of REPO_ROOT, because chef is being run NOT as sudo
 # in order to make homebrew/rvm/etc happy
-file_cache_path "/tmp/chef-file-cache"
+file_cache_path File.join(ENV['HOME'], 'chef-solo')
 cookbook_path [File.join(REPO_ROOT, "cookbooks")]
