eb199e4909d2e1a08d9f3868c8ffe33ca7b7baa5
clifff <clif@sbnation.com>
2011-11-13 15:53:49 -0500

    Move ImageMagick install into Chef

---
+++ cookbooks/image_magick/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Clif Reder"
+maintainer_email "clif@voxmedia.com"
+license          "MIT"
+description      "Installs/configures ImageMagick via homebrew"
+version          "0.1.0"
+
+supports "mac_os_x"
