e2c227f6dd9a85d449e3f89e8fc13e379b57e74f
Clif Reeder <clif@sbnation.com>
2011-11-14 01:07:40 -0500

    fix path for writing my.cnf tempalte

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -11,7 +11,8 @@ end
 
 # Would be nice to have this as a template, but it needs to be run as sudo and chef is called as a normal user
 sudo "populate /etc/my.cnf" do
-  command "cp #{cookbook_path}/mysql/templates/default/my.cnf /etc/my.cnf"
+  templates_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "templates"))
+  command "cp #{templates_path}/default/my.cnf /etc/my.cnf"
   not_if { File.exists?("/etc/my.cnf") }
 end
 
