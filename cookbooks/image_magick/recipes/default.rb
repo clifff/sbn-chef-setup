0fb37623f04851cd777f6344f6622215e9221308
Clif Reeder <clif@sbnation.com>
2011-11-13 20:55:53 -0500

    ignore image_magick install failure cause it is a lie

--- cookbooks/image_magick/recipes/default.rb
+++ cookbooks/image_magick/recipes/default.rb
@@ -3,4 +3,6 @@ include_recipe "homebrew"
 # Unclear why ImageMagick requires me to pin down a version, but such is life
 package "ImageMagick" do
   version "6.7.1-1"
+  # If already installed, specifying version makes this return failure for some weird reason. Ignore it :(
+  ignore_failure true
 end
