a029c5220cdd7159d80b9420533d931026dab63e
Clif Reeder <clif@sbnation.com>
2012-05-21 10:47:15 -0400

    updating rvm version for sbn

--- cookbooks/rvm/attributes/default.rb
+++ cookbooks/rvm/attributes/default.rb
@@ -1,5 +1,5 @@
 default.rvm[:user]   = ENV['SUDO_USER'] || ENV['USER']
 default.rvm[:prefix] = "#{ENV['HOME']}/."
-default.rvm[:default_gemset] = "ree-1.8.7-2010.02@sbn"
+default.rvm[:default_gemset] = "ree-1.8.7-2011.03@sbn"
 default.rvm[:sbn_rubygems_version] = "1.3.7"
 default.rvm[:sbn_bundler_version] = "1.0.15"
